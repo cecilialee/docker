0dc4d7f2aaaa973b9f1dc3fe0f1a2e02874400bc
Xiangdong Gu <xgu@massmutual.com>
2018-12-05 23:48:38 +0800

    new shiny docker file

---
+++ shiny/init.R
@@ -0,0 +1,21 @@
+
+rfile <- commandArgs(trailingOnly = TRUE)[1]
+
+repos <- yaml::yaml.load_file(rfile)
+
+# Download all repos and config .Renviron--------------------------------------
+for (app in names(repos)) {
+  config <- repos[[app]]
+  # Clone repo
+  clone_cmd <- sprintf(
+    "git clone %s %s", config$src, app 
+  )
+  system(clone_cmd)
+  # Create .Renviron for the app
+  envs <- config$envs
+  envs <- mapply(function(x, y) paste(x, y, sep = "="),
+    names(envs), unlist(envs))
+  envs <- paste(envs, collapse = "\n")
+  write(envs, file = sprintf("%s/.Renviron", app))
+}
+
